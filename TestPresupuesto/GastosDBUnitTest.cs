1901261e029f374619ea7af47b9e9ab2887d8074
MedranoC <cristian.medrano@payretailers.com>
2023-07-26 15:48:35 -0300

    se agrego GastoDBUnitTest

---
+++ TestPresupuesto/GastosDBUnitTest.cs
@@ -0,0 +1,87 @@
+﻿using Domain.Dtos.Cliente;
+using Moq;
+using Presupuesto.DataBase;
+using Presupuesto.Repository;
+using System.Data;
+using System.Reflection.PortableExecutable;
+
+namespace TestPresupuesto
+{
+    public class GastosDBUnitTest
+    {
+        private readonly Mock<IConnection> _connection;
+        private readonly GastosDB _gastosDB;
+
+        public GastosDBUnitTest()
+        {
+            _connection = new Mock<IConnection>();
+            _gastosDB = new GastosDB(_connection.Object);
+        }
+
+        [Fact]
+        public async Task GastosPorMesAño_ok()
+        {
+            //Arrange
+            var readerMock = new Mock<IDataReader>();
+            readerMock.Setup(reader => reader.GetOrdinal("Id")).Returns(0);
+            readerMock.Setup(reader => reader.GetString(0)).Returns("ABC1234567");
+
+            readerMock.Setup(reader => reader.GetOrdinal("IdPresupuesto")).Returns(1);
+            readerMock.Setup(reader => reader.GetInt32(1)).Returns(123456);
+
+            readerMock.Setup(reader => reader.GetOrdinal("Gasto")).Returns(2);
+            readerMock.Setup(reader => reader.GetDecimal(2)).Returns(1000);
+
+            readerMock.Setup(reader => reader.GetOrdinal("Usuario")).Returns(3);
+            readerMock.Setup(reader => reader.GetString(3)).Returns("pepito");
+
+            readerMock.Setup(reader => reader.GetOrdinal("FechaCreacion")).Returns(4);
+            readerMock.Setup(reader => reader.GetDateTime(4)).Returns(new DateTime(2023, 07, 10, 00,00,00));
+
+            readerMock.Setup(reader => reader.GetOrdinal("Mes")).Returns(5);
+            readerMock.Setup(reader => reader.GetInt32(5)).Returns(07);
+
+            readerMock.Setup(reader => reader.GetOrdinal("Anio")).Returns(6);
+            readerMock.Setup(reader => reader.GetInt32(6)).Returns(2023);
+
+            //
+            var parametersMock = new Mock<IDbDataParameter>();
+            parametersMock.Setup(p => p.ParameterName).Returns("@mes");
+            parametersMock.Setup(v => v.Value).Returns(07);
+
+            parametersMock.Setup(p => p.ParameterName).Returns("@anio");
+            parametersMock.Setup(v => v.Value).Returns(2023);
+
+            var parameters = new Mock<IDataParameterCollection>();
+            parameters.Setup(command => command.Add(parametersMock.Object));
+
+            //
+            var fecha = "2023-07";
+            var response = new List<Gastos>()
+            {
+                new Gastos()
+                {
+                    Id = "ABC1234567",
+                    IdPresupuesto = 123456,
+                    Gasto = 1000,
+                    Usuario ="pepito",
+                    FechaCreacion = new DateTime(2023, 07, 10, 00,00,00),
+                    Mes = 07,
+                    Año = 2023,
+                }
+            };
+
+            //
+            var connectionMock = MockDependencies.GetConnectionMock(readerMock, parameters);
+            _connection.Setup(x => x.ObtenerConexion()).Returns(connectionMock.Object);
+
+            //Act
+            var res = await _gastosDB.GastosPorMesAño(fecha);
+
+            //Assert
+            Assert.Equal(response[0].Id, res[0].Id);
+         
+        }
+
+    }
+}
