4922e495a824746f5cce93f9308ce5953af7ef64
Cristian Medrano <cristian.medrano@payretailers.com>
2023-07-27 12:20:33 -0300

    test de archivo presuestoDB

--- TestPresupuesto/PresupuestosDBUnitTest.cs
+++ TestPresupuesto/PresupuestosDBUnitTest.cs
@@ -17,6 +17,73 @@ namespace TestPresupuesto
             _connection = new Mock<IConnection>();
             _presupuestosDB = new PresupuestosDB(_connection.Object);
         }
+        [Fact]
+        public async Task AgregarPresupuesto_OK()
+        {
+
+            var request = new RequestPresupuesto()
+            {
+
+                Usuario = "cristian",
+                DuracionPresupuesto = 30,
+
+                detallePresupuesto = new List<DetallePresupuesto>()
+                {
+                    new DetallePresupuesto
+                    {
+                        IdRubro = "IND",
+                        Presupuesto =30000,
+                        Rubro = "INDUMENTARIA"
+                        
+
+                    }
+                }
+            };
+            var readerMock = new Mock<IDataReader>();
+            readerMock.Setup(reader => reader.GetOrdinal("@idPresupuesto")).Returns(0);
+            readerMock.Setup(reader => reader.GetInt32(0)).Returns(123456);
+
+            readerMock.Setup(reader => reader.GetOrdinal("@idRubro")).Returns(1);
+            readerMock.Setup(reader => reader.GetString(1)).Returns("IND");
+
+            readerMock.Setup(reader => reader.GetOrdinal("@rubro")).Returns(2);
+            readerMock.Setup(reader => reader.GetString(2)).Returns("INDUMENTARIA");
+
+            readerMock.Setup(reader => reader.GetOrdinal("@usuario")).Returns(3);
+            readerMock.Setup(reader => reader.GetString(3)).Returns("cristian");
+
+            readerMock.Setup(reader => reader.GetOrdinal("@presupuesto")).Returns(4);
+            readerMock.Setup(reader => reader.GetDecimal(4)).Returns(15000);
+
+            readerMock.Setup(reader => reader.GetOrdinal("@gastado")).Returns(5);
+            readerMock.Setup(reader => reader.GetDecimal(5)).Returns(3000);
+
+            readerMock.Setup(reader => reader.GetOrdinal("@fechaDeCreacion")).Returns(6);
+            readerMock.Setup(reader => reader.GetDateTime(6)).Returns(new DateTime(2016, 6, 1, 6, 34, 53));
+
+            readerMock.Setup(reader => reader.GetOrdinal("@mes")).Returns(7);
+            readerMock.Setup(reader => reader.GetInt32(7)).Returns(05);
+
+            readerMock.Setup(reader => reader.GetOrdinal("@anio")).Returns(8);
+            readerMock.Setup(reader => reader.GetInt32(8)).Returns(2023);
+            //
+
+            var parameters = new Mock<IDataParameterCollection>();
+
+            var connectionMock = MockDependencies.GetConnectionMock(readerMock, parameters);
+            _connection.Setup(x => x.ObtenerConexion())
+                .Returns(connectionMock.Object);
+
+            //ACT
+           var res = await _presupuestosDB.AgregarPresupuesto(request);
+
+            // Assert
+            Assert.NotNull(res);    
+
+        }
+
+
+
 
         [Fact]
         public async Task PresupuestoPorFecha_Ok()
@@ -100,7 +167,7 @@ namespace TestPresupuesto
             var estadoPresupuesto = new EstadoPresupuesto()
             {
                 Rubro = "INDUMENTARIA",
-                Disponible = 3000
+                Disponible = 2000
             };
 
             //
@@ -109,7 +176,7 @@ namespace TestPresupuesto
             readerMock.Setup(reader => reader.GetString(0)).Returns("INDUMENTARIA");
 
             readerMock.Setup(reader => reader.GetOrdinal("Presupuesto")).Returns(1);
-            readerMock.Setup(reader => reader.GetDecimal(1)).Returns(30000);
+            readerMock.Setup(reader => reader.GetDecimal(1)).Returns(3000);
 
             readerMock.Setup(reader => reader.GetOrdinal("Gastado")).Returns(2);
             readerMock.Setup(reader => reader.GetDecimal(2)).Returns(1000);
@@ -131,7 +198,7 @@ namespace TestPresupuesto
             var res = await _presupuestosDB.SaldoDisponible("123456");
 
             //Assert
-           // Assert.Equal(, res[0].Disponible);
+            Assert.Equal(estadoPresupuesto.Disponible, res[0].Disponible);
         }
 
 
